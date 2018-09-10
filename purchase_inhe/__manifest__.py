e684cfbc88d7280d9795ff3f53b2eeead4e2e6a0
hrishikesh1688 <hrishikesh@saiaipl.com>
2018-09-10 15:32:28 +0530

    Purchase folder added

---
+++ purchase_inhe/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'my_pucrhase_inhe',
+    'version': '11.0.1.0.0',
+    'category': 'my_category',
+    'author': 'SAI AIPL',
+    'depends': ['purchase','sale','sunfire_sales'],
+    'data': [
+        'security/security_groups.xml',
+        'views/purchase_view.xml',
+        'wizard/create_purchase_order_view.xml',
+        'views/sale_invoice_list.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
